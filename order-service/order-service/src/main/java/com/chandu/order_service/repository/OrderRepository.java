62a772d8f30be3c1d5d6cff74c044b9ebc800ff6
Chandu <116370335+Chandu373@users.noreply.github.com>
2024-11-09 20:35:33 +0530

    Delete order-service directory

--- order-service/order-service/src/main/java/com/chandu/order_service/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.chandu.order_service.repository;
-
-import com.chandu.order_service.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order,Long> {
-}
