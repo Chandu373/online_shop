47db93afcbd4fa48b4f5b169dec04381cb17dbe1
Chandu <116370335+Chandu373@users.noreply.github.com>
2024-11-10 16:28:30 +0530

    security-config

---
+++ online-shop-microservices/discovery-server/src/main/java/com/chandu/discovery_server/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.chandu.discovery_server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  {
+}
